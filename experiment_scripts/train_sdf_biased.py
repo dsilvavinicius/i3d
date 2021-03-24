6f9c945a9445868e286a01c7696596d083948b7b
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-03-24 16:24:44 -0300

    Moved the samplers to a separate file.

--- experiment_scripts/train_sdf_biased.py
+++ experiment_scripts/train_sdf_biased.py
@@ -16,24 +16,6 @@ import loss_functions
 import modules
 
 
-class CurvatureSeqSampler(Sampler):
-    """A simple sampler that orders the data samples by curvature and returns
-    them in sequence.
-
-    Parameters
-    ----------
-    data_source: PointCloud
-        The actual data source. Must have the curvatures as features.
-    """
-    def __init__(self, data_source):
-        self.data_source = data_source
-        self.sorted_curvatures_idx = np.argsort(np.absolute(self.data_source.curvatures))
-
-    def __iter__(self):
-        return iter(range(len(self.sorted_curvatures_idx)))
-
-    def __len__(self):
-        return len(self.data_source)
 
 
 p = ArgumentParser()
