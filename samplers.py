068b7cf4f6ce665ce80cae9c7b7163910be537be
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-03-25 10:15:53 -0300

    Minor sampler updates.

--- samplers.py
+++ samplers.py
@@ -34,10 +34,12 @@ class CurvatureHistogramSampler(Sampler):
     data_source: PointCloud
         The actual data source. Must have the curvatures as features.
     """
-    def __init__(self, data_source):
+    def __init__(self, data_source, bins=10):
         self.data_source = data_source
-        curvatures = (self.data_source.min_curvatures + self.data_source.max_curvatures) / 2
-        self.sorted_curvatures_idx = np.argsort(np.absolute(curvatures))
+        mean_curvatures = (self.data_source.min_curvatures + self.data_source.max_curvatures) / 2
+        hist = np.histogram(mean_curvatures, bins=bins)
+        self.weights = hist / sum(hist)
+        self.sorted_curvatures_idx = np.argsort(np.absolute(mean_curvatures))
 
     def __iter__(self):
         return iter(range(len(self.sorted_curvatures_idx)))
