9d09407551bb427fd8886dbc70e7d4a78ef90f4e
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-05-07 13:46:40 -0300

    Adding tests for empty point clouds.

--- view_pointcloud.py
+++ view_pointcloud.py
@@ -32,36 +32,47 @@ surf_samples = samples[samples[:, -1] == 0, :]
 int_samples = samples[samples[:, -1] < 0, :]
 ext_samples = samples[samples[:, -1] > 0, :]
 
-surf_cloud = pyrender.Mesh.from_points(
-    points=surf_samples[:, :3],
-    normals=surf_samples[:, 3:6],
-    colors=np.zeros((surf_samples.shape[0], 3))
-)
+print("Samples on surface: ", surf_samples.shape[0])
+print("Samples with SDF < 0: ", int_samples.shape[0])
+print("Samples with SDF > 0: ", ext_samples.shape[0])
 
-int_cloud = pyrender.Mesh.from_points(
-    points=int_samples[:, :3],
-    normals=int_samples[:, 3:6],
-    colors=np.zeros((int_samples.shape[0], 3)) + [1, 0, 0]
-)
 
-ext_cloud = pyrender.Mesh.from_points(
-    points=ext_samples[:, :3],
-    normals=ext_samples[:, 3:6],
-    colors=np.zeros((ext_samples.shape[0], 3)) + [0, 1, 0]
-)
+scene = pyrender.Scene()
+registered_keys = {}
+
+if surf_samples.shape[0] > 0:
+    surf_cloud = pyrender.Mesh.from_points(
+        points=surf_samples[:, :3],
+        normals=surf_samples[:, 3:6],
+        colors=np.zeros((surf_samples.shape[0], 3))
+    )
+    scene.add(surf_cloud)
+    registered_keys["u"] = (toggle_mesh_vis, [surf_cloud])
+
+if int_samples.shape[0] > 0:
+    int_cloud = pyrender.Mesh.from_points(
+        points=int_samples[:, :3],
+        normals=int_samples[:, 3:6],
+        colors=np.zeros((int_samples.shape[0], 3)) + [1, 0, 0]
+    )
+    scene.add(int_cloud)
+    registered_keys["i"] = (toggle_mesh_vis, [int_cloud])
+
+if ext_samples.shape[0] > 0:
+    ext_cloud = pyrender.Mesh.from_points(
+        points=ext_samples[:, :3],
+        normals=ext_samples[:, 3:6],
+        colors=np.zeros((ext_samples.shape[0], 3)) + [0, 1, 0]
+    )
+    scene.add(ext_cloud)
+    registered_keys["e"] = (toggle_mesh_vis, [ext_cloud])
 
 light = pyrender.PointLight(intensity=500)
 
-scene = pyrender.Scene()
-scene.add(surf_cloud)
-scene.add(int_cloud)
-scene.add(ext_cloud)
 scene.add(light)
 viewer = pyrender.Viewer(
     scene,
     point_size=2,
     use_raymond_lighting=True,
-    registered_keys={"u": (toggle_mesh_vis, [surf_cloud]),
-                     "i": (toggle_mesh_vis, [int_cloud]),
-                     "e": (toggle_mesh_vis, [ext_cloud])}
+    registered_keys=registered_keys
 )
