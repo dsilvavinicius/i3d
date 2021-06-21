8e629e4ace588a5d5772c39f8316bda514ceb536
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-06-21 14:47:03 -0300

    Renaming folder.

---
+++ tools/train_armadillo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#python experiment_scripts/train_sdf_preproc.py --model_type=sine --point_cloud_path=data/armadillo.xyz --batch_size=4096 --experiment_name=home_armadillo_$(date --iso-8601=m) --num_epochs=1000 -w 5
+#python experiment_scripts/train_sdf_preproc.py --model_type=sine --point_cloud_path=data/armadillo.xyz --batch_size=4096 --experiment_name=home_armadillo_$(date --iso-8601=m) --num_epochs=1000 -w 30
+#python experiment_scripts/train_sdf_preproc.py --model_type=sine --point_cloud_path=data/armadillo.xyz --batch_size=4096 --experiment_name=home_armadillo_$(date --iso-8601=m) --num_epochs=1000 -w 60
