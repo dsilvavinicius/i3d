3fd6a4cf6d9b471aae84dd53b2361b12283278e5
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-10-07 09:14:31 -0300

    Removing the 4D experiments and some unused options.

--- experiment_scripts/test_sdf.py
+++ experiment_scripts/test_sdf.py
@@ -24,31 +24,24 @@ p.add_argument('--experiment_name', type=str, required=True,
 p.add_argument('--batch_size', type=int, default=16384)
 p.add_argument('--checkpoint_path', default=None, help='Checkpoint to trained model.')
 
-p.add_argument('--model_type', type=str, default='sine',
-               help='Options are "sine" (all sine activations) and "mixed" (first layer sine, other layers tanh)')
-p.add_argument('--mode', type=str, default='mlp',
-               help='Options are "mlp" or "nerf"')
-p.add_argument('--resolution', type=int, default=1600)
+p.add_argument('--resolution', type=int, default=256)
 p.add_argument('--time', action='store_true', required=False, help='Indicates that time will also be considered')
 
 opt = p.parse_args()
 
+in_features = 3
 
-in_features = 0
-
-if(opt.time == True):
-    in_features = 4
-else:
-    in_features = 3
 
 class SDFDecoder(torch.nn.Module):
     def __init__(self):
         super().__init__()
         # Define the model.
-        if opt.mode == 'mlp':
-            self.model = modules.SingleBVPNet(type=opt.model_type, final_layer_factor=1, in_features=in_features)
-        elif opt.mode == 'nerf':
-            self.model = modules.SingleBVPNet(type='relu', mode='nerf', final_layer_factor=1, in_features=in_features)
+        self.model = modules.SingleBVPNet(
+            typ="sine",
+            final_layer_factor=1,
+            in_features=in_features,
+            num_hidden_layers=3,
+        )
         self.model.load_state_dict(torch.load(opt.checkpoint_path))
         self.model.cuda()
 
@@ -64,10 +57,4 @@ sdf_decoder = SDFDecoder()
 root_path = os.path.join(opt.logging_root, opt.experiment_name)
 utils.cond_mkdir(root_path)
 
-if(opt.time == True):
-    N = 5 #number of samples of the interval time [0,1]
-    for i in range(N):
-        t = i/(N-1)
-        sdf_meshing.create_mesh(sdf_decoder, os.path.join(root_path, f"test_epoch_{t}"), t, N=opt.resolution)
-else:
-    sdf_meshing.create_mesh_with_curvatures(sdf_decoder, os.path.join(root_path, 'test'), N=opt.resolution)
+sdf_meshing.create_mesh_with_curvatures(sdf_decoder, os.path.join(root_path, 'test'), N=opt.resolution)
