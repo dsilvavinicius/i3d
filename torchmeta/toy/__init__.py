f183b8f5751d313117f6056a5bd239e47f9de074
Guilherme G. Schardong <schardong.gg@gmail.com>
2022-02-12 19:18:51 -0300

    Removing torchmeta code.

--- torchmeta/toy/__init__.py
+++
@@ -1,7 +0,0 @@
-from torchmeta.toy.harmonic import Harmonic
-from torchmeta.toy.sinusoid import Sinusoid
-from torchmeta.toy.sinusoid_line import SinusoidAndLine
-
-from torchmeta.toy import helpers
-
-__all__ = ['Harmonic', 'Sinusoid', 'SinusoidAndLine', 'helpers']
