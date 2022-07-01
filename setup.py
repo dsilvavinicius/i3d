145bdeaaf186c902501263edb7bd0b39821cad00
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2022-07-01 17:12:47 +0100

    Added a simple setup script to import the classes for testing.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name="i3d",
+    version="0.0.1",
+    description="Differential Geometry for Neural Implicits",
+    author="The Deep Implicits Group",
+    packages=["."],
+)
