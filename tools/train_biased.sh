3c61e8524a54fd9424841659d3984c5564f6ae08
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2022-07-29 14:34:25 +0100

    Removing old, non-functional scripts.

--- tools/train_biased.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python experiment_scripts/train_sdf_biased.py --model_type=sine --point_cloud_path=data/test_big_4.xyz --batch_size=2048 --experiment_name=home_biasedsampler_$(date --iso-8601=m) --num_epochs=100
