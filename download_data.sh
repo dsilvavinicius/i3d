f99125c4d594189483b54a7901b04aa87ac54a57
Guilherme Schardong <schardong.gg@gmail.com>
2022-09-04 20:26:29 +0100

    Added script to download the data from google drive.

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+fileid=$1
+filename=$2
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
