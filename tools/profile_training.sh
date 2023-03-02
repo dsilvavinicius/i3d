15e5de0b67887e51cd55080daf269d5e64d3293d
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2023-03-02 12:03:00 +0000

    Moved the profiler script to `tools`. Added the analysis notebook.

--- tools/profile_training.sh
+++ tools/profile_training.sh
@@ -2,7 +2,7 @@
 
 for i in {1..10}; do
     echo "Run ${i}"
-    basename=run_${i}_armadillo_100epochs_curvatures2
+    basename=run_${i}_armadillo_100epochs_curvatures
     python -m cProfile -o ${basename}.prof experiment_scripts/sdf_for_n_iters.py > ${basename}.log
     gprof2dot -f pstats ${basename}.prof | dot -Tpng -o ${basename}.png
 done
